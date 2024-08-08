3b457306e6ec3827dd73b9705798a93d24e6bd2a
Sandaruwan Rathnayaka <153787764+MaxDoom57@users.noreply.github.com>
2024-08-08 22:21:49 +0530

    Initial commit with React project

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-blue-500 w-screen h-screen">
+      <div className="bg-green-500 w-screen h-12"></div>
+      <div className="bg-green-800 w-screen h-[500px]"></div>
+      <div className="bg-orange-500 w-screen h-24"></div>
+      <div className="bg-orange-800 w-screen h-[360px]"></div>
+    </div>
+  );
+};
+
+export default Home;
