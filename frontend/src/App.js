2599153881337568db76d2e426e9214b0ae84d2c
Asith Lakmal <asithalakmal1999@gmail.com>
2024-08-12 23:17:13 +0530

    StaffList page call from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import Home from "./Pages/Home";
+import StaffList from "./Pages/StaffList";
 import Student from "./Pages/Student";
 
 function App() {
   return (
     <div>
       {/* <Home /> */}
-      <Student/>
+      {/* <Student/> */}
+      <StaffList/>
     </div>
   );
 }
